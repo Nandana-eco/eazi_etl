29b1a9a60c85f3100ba9b9b8ac5ab9bc9c28aa65
Nandana-eco <Nandana.sajithkumar@ecosavepay.com>
2026-04-02 10:42:04 +0100

    fifth commit

--- config.py
+++ config.py
@@ -1,8 +1,25 @@
 TABLES = [
     
     {
+        "mysql_table": "client_meters",
+        "pg_table": "raw.client_meters"
+    },
+     {
         "mysql_table": "client_status",
         "pg_table": "raw.client_status"
+    },
+     {
+        "mysql_table": "clients",
+        "pg_table": "raw.clients"
+    },
+     {
+        "mysql_table": "company",
+        "pg_table": "raw.company"
+    },
+     {
+        "mysql_table": "active_users",
+        "pg_table": "raw.active_users"
     }
+    
    
 ]
